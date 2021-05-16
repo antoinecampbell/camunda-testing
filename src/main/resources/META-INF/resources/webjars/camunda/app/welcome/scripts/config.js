9c09acefe641e40ba37b5b4a32bf51a92f16e973
Antoine Campbell <antoine.s.campbell@gmail.com>
2021-05-16 14:25:38 -0400

    Secure UI applications with Okta SSO

---
+++ src/main/resources/META-INF/resources/webjars/camunda/app/welcome/scripts/config.js
@@ -0,0 +1,9 @@
+export default {
+    links: [
+        {
+            label: 'Login with your Okta Credentials',
+            href: '/oauth2/authorization/okta',
+            description: 'Link to Okta login'
+        }
+    ]
+};

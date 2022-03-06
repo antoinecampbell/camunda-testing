a6a735994ab2b095bcb93adb07a1999f8c2dd047
Antoine Campbell <antoine.s.campbell@gmail.com>
2022-03-06 16:48:00 -0500

    Update bucket to be private

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,6 +31,14 @@ resource "aws_s3_bucket" "lambda" {
   tags = merge(merge(local.tags, { Name : local.bucket_name }))
 }
 
+resource "aws_s3_bucket_public_access_block" "lambda" {
+  bucket = aws_s3_bucket.lambda.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_acl" "lambda" {
   bucket = aws_s3_bucket.lambda.id
   acl = "private"
